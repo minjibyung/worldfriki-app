d283c60f79a95dd91fcf0da7902ad88c51571593
minjibyung <vsanchezrestrepo04@gmail.com>
2025-06-04 16:19:07 -0500

    pruebas

--- src/components/pages/peliculas/Movie.jsx
+++ src/components/pages/peliculas/Movie.jsx
@@ -1,147 +1,138 @@
-import React from "react";
-import Categoria1 from "../../layouts/categorias/Categoria1";
-import SliderMovies from "../../layouts/slider/SliderMovies";
-import Search1 from "../../layouts/search/Search1";
+// import React from "react";
+// import Categoria1 from "../../layouts/categorias/Categoria1";
+// import SliderMovies from "../../layouts/slider/SliderMovies";
+// import Search1 from "../../layouts/search/Search1";
 
-export const Movie = () => {
-    return(
-        <>
-        <Search1/>
-            <Categoria1 />
 
-<h1 className="title">Peliculas</h1>
-<section className="Aside">
-    <aside className="Aside1" category="accion">
-        <h5>Avengers: Era de ultròn</h5>
-        <a href="https://www.youtube.com/watch?v=DMFBm_lp4rU" target="_blank "><img src="https://res.cloudinary.com/dhypxlezc/image/upload/v1687637207/WorldFriki/peliculas/avengers_sir7ux.jpg"/></a>
-{/* <!-- <h5>Sipnosis</h5>           
-<p>Cuando un niño desaparece, sus amigos,
-la familia y la policía se ven envueltos 
-en una serie de eventos misteriosos al 
-tratar de encontrarlo. Su ausencia coincide 
-con el avistamiento de una criatura terrorífica 
-y la aparición de una extraña niña.</p><br>
-<li>Cantidad de temporadas: 4</li> --> */}
-</aside>
-<aside className="Aside1" category="suspenso">
-        <h5>Joker</h5>
-        <a href="https://www.youtube.com/watch?v=EIyZqNbZQI8" target="_blank "><img src="https://res.cloudinary.com/dhypxlezc/image/upload/v1687648132/WorldFriki/peliculas/joker_hpley4.jpg"/></a>
-{/* <!-- <h5>Sipnosis</h5>
-<p>Un cirujano joven y autista que padece el síndrome del sabio empieza a trabajar en un hospital prestigioso.
-Allá tendrá que vencer el escepticismo con el que sus colegas lo reciben.</p><br>
-<li>Cantidad de temporadas: 5</li>  --> */}
-    </aside>
-    <aside className="Aside1" category="accion">
-    <h5>Top Gun: Pasion y Gloria</h5>
-    <a href="https://www.panamericana.com.co/misery-3/p" target="_blank "><img src="https://res.cloudinary.com/dhypxlezc/image/upload/v1687648134/WorldFriki/peliculas/topgun_muzg2s.jpg"/></a> 
+// const booksData = [
+//     {
+//       title: "Stranger Things",
+//       category: "suspenso",
+//       img: "https://res.cloudinary.com/dhypxlezc/image/upload/v1687030696/WorldFriki/series/strangerthings1_jiue4t.webp",
+//       link: "https://www.panamericana.com.co/el-resplandor/p"
+//     },
+//      {
+//       title: "The Good Doctor",
+//       category: "accion",
+//       img: "https://res.cloudinary.com/dhypxlezc/image/upload/v1687030696/WorldFriki/series/thegooddoctor_hyy6vq.jpg",
+//       link: "https://www.casadellibro.com/libro-el-visitante/9788401021190/7099713"
+//     },
+//      {
+//       title: "Juego De Tronos",
+//       category: "accion",
+//       img: "https://res.cloudinary.com/dhypxlezc/image/upload/v1687030688/WorldFriki/series/juegodetronos_hddcbs.jpg",
+//       link: "https://www.casadellibro.com/libro-el-visitante/9788401021190/7099713"
+//     },
+//      {
+//       title: "Pretty Little Liars",
+//       category: "suspenso",
+//       img: "https://res.cloudinary.com/dhypxlezc/image/upload/v1687030693/WorldFriki/series/prettylittleliars_cwx9vc.jpg",
+//       link: "https://www.casadellibro.com/libro-el-visitante/9788401021190/7099713"
+//     }, 
+//     {
+//       title: "Peaky Blinders",
+//       category: "accion",
+//       img: "https://res.cloudinary.com/dhypxlezc/image/upload/v1687030698/WorldFriki/series/peakybleanders_hsppqo.webp",
+//       link: "https://www.casadellibro.com/libro-el-visitante/9788401021190/7099713"
+//     },
+//      {
+//       title: "The End Of The F***ing World",
+//       category: "accion",
+//       img: "https://res.cloudinary.com/dhypxlezc/image/upload/v1687030696/WorldFriki/series/fuckingworld_tfbimu.webp",
+//       link: "https://www.casadellibro.com/libro-el-visitante/9788401021190/7099713"
+//     },
+//      {
+//       title: "Riverdale",
+//       category: "suspenso",
+//       img: "https://res.cloudinary.com/dhypxlezc/image/upload/v1687030697/WorldFriki/series/riverdale_cbpi38.jpg",
+//       link: "https://www.casadellibro.com/libro-el-visitante/9788401021190/7099713"
+//     },
+//      {
+//       title: "Gambito De Dama",
+//       category: "accion",
+//       img: "https://res.cloudinary.com/dhypxlezc/image/upload/v1687030686/WorldFriki/series/gambitodedama_qmlxnt.jpg",
+//       link: "https://www.casadellibro.com/libro-el-visitante/9788401021190/7099713"
+//     },
+//      {
+//       title: "The Sex Education",
+//       category: "comedia",
+//       img: "https://res.cloudinary.com/dhypxlezc/image/upload/v1687030695/WorldFriki/series/sexeducation_ulsh1v.jpg",
+//       link: "https://www.casadellibro.com/libro-el-visitante/9788401021190/7099713"
+//     },
+//      {
+//       title: "Euphoria",
+//       category: "suspenso",
+//       img: "https://res.cloudinary.com/dhypxlezc/image/upload/v1687030693/WorldFriki/series/EUPHORIA_brtm3y.jpg",
+//       link: "https://www.casadellibro.com/libro-el-visitante/9788401021190/7099713"
+//     },
+//      {
+//       title: "Elite",
+//       category: "suspenso",
+//       img: "https://res.cloudinary.com/dhypxlezc/image/upload/v1687030691/WorldFriki/series/ELITE_nrs3di.jpg",
+//       link: "https://www.casadellibro.com/libro-el-visitante/9788401021190/7099713"
+//     },
+//      {
+//       title: "Mr. Robot",
+//       category: "suspenso",
+//       img: "https://res.cloudinary.com/dhypxlezc/image/upload/v1687030691/WorldFriki/series/mr.robot_fmmddv.jpg",
+//       link: "https://www.casadellibro.com/libro-el-visitante/9788401021190/7099713"
+//     },
+//      {
+//       title: "La Maldicion De La Hillhouse",
+//       category: "terror",
+//       img: "https://res.cloudinary.com/dhypxlezc/image/upload/v1687030689/WorldFriki/series/maldicion_p9agyi.jpg",
+//       link: "https://www.casadellibro.com/libro-el-visitante/9788401021190/7099713"
+//     },
+//      {
+//       title: "La Chica Nueva",
+//       category: "suspenso",
+//       img: "https://res.cloudinary.com/dhypxlezc/image/upload/v1687030688/WorldFriki/series/lachicanueva_lqbimx.jpg",
+//       link: "https://www.casadellibro.com/libro-el-visitante/9788401021190/7099713"
+//     },
+//      {
+//       title: "Vagabond",
+//       category: "accion",
+//       img: "https://res.cloudinary.com/dhypxlezc/image/upload/v1687030686/WorldFriki/series/VAGABOND_et2h0y.webp",
+//       link: "https://www.casadellibro.com/libro-el-visitante/9788401021190/7099713"
+//     },
+//      {
+//       title: "Young Royals",
+//       category: "Romance",
+//       img: "https://res.cloudinary.com/dhypxlezc/image/upload/v1749070121/WorldFriki/series/youngroyals_ce7nlx.jpg",
+//       link: "https://www.casadellibro.com/libro-el-visitante/9788401021190/7099713"
+//     },
+//     ];
 
-{/* <!-- <h5>Sipnosis</h5>
-<p>Dos familias poderosas, reyes y reinas, caballeros y renegados, hombres falsos y honestos, haciendo
-parte de un juego mortal por el control de los Siete Reinados de Westeros y por sentarse en el trono
-más alto. Martin es el co productor ejecutivo y uno de los escritores de la serie que fue filmada
-en el Norte de Irlanda y Malta.</p><br>
-<li>Cantidad de temporadas: 8</li> --> */}
-</aside>
-<aside className="Aside1" category="accion">
-    <h6>Top Gun: Maverick</h6>
-    <a href="https://www.panamericana.com.co/cementerio-de-animales/p" target="_blank "><img src="https://res.cloudinary.com/dhypxlezc/image/upload/v1687648134/WorldFriki/peliculas/maverick_ihfoak.jpg"/></a> 
 
-{/* <!-- <h5>Sipnosis</h5> -->
-<!-- <p>Han pasado tres años desde que Alison quien era la reina del grupo se desapareció. Spencer, Aria, Hanna y Emily
-continúan sus vidas aunque se han apartado. Ahora en la secundaria, cada chica está enfrentando diferentes retos
-cuando mensajes de texto anónimos de "A" amenazan con exponer todos sus secretos. ¿Quién es "A" y cómo sabe tanto?.
-Basada en la series de novelas de Sara Shepard.</p><br>
-<li>Último episodio: 27 de junio de 2017</li>    
-<li>Cantidad de temporadas: 7</li>
-<li>Duración: 41-80 minutos</li> --> */}
-</aside>
-<aside className="Aside1" category="romance">
-    <h5>After: Amor infinito</h5>
-    <a href="https://www.amazon.com/-/es/Stephen-King/dp/8466357238" target="_blank "><img src="https://res.cloudinary.com/dhypxlezc/image/upload/v1687648132/WorldFriki/peliculas/after_t51sm9.jpg"/></a> 
+// export const Movie = () => {
+//      const [selectedCategory, setSelectedCategory] = useState("all");
+      
+//         const filteredBooks = selectedCategory === "all"
+//           ? booksData
+//           : booksData.filter(book => book.category === selectedCategory);
 
-{/* <!-- <h5>Sipnosis</h5> -->
-<!-- <p>Han pasado tres años desde que Alison quien era la reina del grupo se desapareció. Spencer, Aria, Hanna y Emily
-continúan sus vidas aunque se han apartado. Ahora en la secundaria, cada chica está enfrentando diferentes retos
-cuando mensajes de texto anónimos de "A" amenazan con exponer todos sus secretos. ¿Quién es "A" y cómo sabe tanto?.
-Basada en la series de novelas de Sara Shepard.</p><br>
-<li>Último episodio: 27 de junio de 2017</li>    
-<li>Cantidad de temporadas: 7</li>
-<li>Duración: 41-80 minutos</li> --> */}
-</aside>
-<aside className="Aside1" category="accion">
-    <h5>Thor: Amor y Trueno</h5>
-    <a href="https://www.casadellibro.com/libro-el-visitante/9788401021190/7099713" target="_blank "><img src="https://res.cloudinary.com/dhypxlezc/image/upload/v1687648135/WorldFriki/peliculas/thor_q8fnj8.jpg"/></a> 
+//     return(
+//         <>
+//         <Search1/>
+//             <Categoria1 
+//              selectedCategory={selectedCategory}
+//             onCategoryClick={setSelectedCategory}
+//             />
 
-{/* <!-- <h5>Sipnosis</h5>
-<p>Han pasado tres años desde que Alison quien era la reina del grupo se desapareció. Spencer, Aria, Hanna y Emily
-continúan sus vidas aunque se han apartado. Ahora en la secundaria, cada chica está enfrentando diferentes retos
-cuando mensajes de texto anónimos de "A" amenazan con exponer todos sus secretos. ¿Quién es "A" y cómo sabe tanto?.
-Basada en la series de novelas de Sara Shepard.</p><br>
-<li>Último episodio: 27 de junio de 2017</li>    
-<li>Cantidad de temporadas: 7</li>
-<li>Duración: 41-80 minutos</li> --> */}
-</aside>
-<aside className="Aside1" category="suspenso">
-    <h5>Zombie Apocalypse</h5>
-    <a href="https://www.panamericana.com.co/doctor-sueno-588241/p" target="_blank "><img src="https://res.cloudinary.com/dhypxlezc/image/upload/v1687648136/WorldFriki/peliculas/zombie_hhmlow.png"/></a> 
 
-{/* <!-- <h5>Sipnosis</h5>
-<p>Han pasado tres años desde que Alison quien era la reina del grupo se desapareció. Spencer, Aria, Hanna y Emily
-continúan sus vidas aunque se han apartado. Ahora en la secundaria, cada chica está enfrentando diferentes retos
-cuando mensajes de texto anónimos de "A" amenazan con exponer todos sus secretos. ¿Quién es "A" y cómo sabe tanto?.
-Basada en la series de novelas de Sara Shepard.</p><br>
-<li>Último episodio: 27 de junio de 2017</li>    
-<li>Cantidad de temporadas: 7</li>
-<li>Duración: 41-80 minutos</li> --> */}
-</aside>
-<aside className="Aside1" category="suspenso">
-    <h5>Fall</h5>
-    <a href="https://www.panamericana.com.co/el-instituto-585646/p" target="_blank "><img src="https://res.cloudinary.com/dhypxlezc/image/upload/v1687637207/WorldFriki/peliculas/fall_w2c3ph.jpg"/></a> 
-
-</aside>
-<aside className="Aside1" category="accion">
-    <h5>La chica salvaje</h5>
-    <a href="https://www.alibrate.com/libro/ojos-de-fuego/59872eadcba2bce50c1f053f" target="_blank "><img src="https://res.cloudinary.com/dhypxlezc/image/upload/v1687637207/WorldFriki/peliculas/selva_mc6qaz.jpg"/></a> 
-
-</aside>
-<aside className="Aside1" category="accion">
-    <h5>Godzilla VS. Kong</h5>
-    <a href="https://www.panamericana.com.co/la-cupula-553539/p" target="_blank "><img src="https://res.cloudinary.com/dhypxlezc/image/upload/v1687648132/WorldFriki/peliculas/godzilla_tpvjvt.jpg"/></a> 
-
-</aside>
-<aside className="Aside1" category="accion">
-    <h5>Beast</h5>
-    <a href="https://libreriatemis.com/product/eleanor-y-park/" target="_blank "><img src="https://res.cloudinary.com/dhypxlezc/image/upload/v1687637208/WorldFriki/peliculas/beast_kdaos5.jpg"/></a> 
-
-</aside>
-<aside className="Aside1" category="accion">
-    <h5>Black Adam</h5>
-    <a href="https://www.buscalibre.com.co/libro-stranger-things-mentes-peligrosas/9788401022975/p/51446345" target="_blank "><img src="https://res.cloudinary.com/dhypxlezc/image/upload/v1687648132/WorldFriki/peliculas/blackadam_sbsxpm.jpg"/></a> 
-
-</aside>
-<aside className="Aside1" category="suspenso">
-    <h5>Revenant: El Renacido</h5>
-    <a href="https://librerianacional.com/producto/stranger-things-mundos-del-reves-la-guia-oficial-" target="_blank "><img src="https://res.cloudinary.com/dhypxlezc/image/upload/v1687648136/WorldFriki/peliculas/renacido_l0pn0v.jpg"/></a> 
-
-</aside>
-<aside className="Aside1" category="accion">
-    <h5>King Kong</h5>
-    <a href="https://www.panamericana.com.co/stranger-things-a-oscuras-en-la-ciudad-621041/p" target="_blank "><img src="https://res.cloudinary.com/dhypxlezc/image/upload/v1687648135/WorldFriki/peliculas/king_qylgj9.jpg"/></a> 
-
-</aside>
-<aside className="Aside1" category="suspenso">
-    <h6>El dia del fin del mundo</h6>
-    <a href="https://www.amazon.com/-/es/Martí-Pallàs/dp/8494791710" target="_blank "><img src="https://res.cloudinary.com/dhypxlezc/image/upload/v1687648132/WorldFriki/peliculas/finmundo_ktsb72.jpg"/></a> 
-
-</aside> 
-<aside className="Aside1" category="accion">
-    <h5>El sorprendente hombre araña</h5>
-    <a href="https://www.penguinlibros.com/co/novela-negra-misterio-y-thriller/25479-ebook-guerra-mundial-z-9788490628096" target="_blank "><img src="https://res.cloudinary.com/dhypxlezc/image/upload/v1687648134/WorldFriki/peliculas/menara%C3%B1a_ofznfn.jpg"/></a> 
-
-</aside>
-<SliderMovies/>
-</section>
-        </>
-    );
-};
-export default Movie;
+//           <h1 className="title">Peliculas</h1>
+//           <section className="Aside">
+//             {filteredBooks.map((book, index) => (
+//               <aside key={index} className="Aside1" category={book.category}>
+//                 <h5>{book.title}</h5>
+//                 <a href={book.link} target="_blank" rel="noopener noreferrer">
+//                   <img src={book.img} alt={book.title} />
+//                 </a>
+//               </aside>
+//             ))}
+// <SliderMovies/>
+// </section>
+//         </>
+//     );
+// };
+// export default Movie;
--- src/components/pages/series/Series.jsx
+++ src/components/pages/series/Series.jsx
@@ -16,82 +16,82 @@ export const Series = () => {
         <aside className="Aside1" id="suspenso">
             <h5>Stranger Things</h5>
             <a href="https://www.youtube.com/watch?v=220ClSxwhxY" target="_blank" rel="noopener noreferrer">
-                <img src="https://res.cloudinary.com/dhypxlezc/image/upload/v1687030696/WorldFriki/strangerthings1_jiue4t.webp" alt="Stranger Things"/></a>
+                <img link="https://res.cloudinary.com/dhypxlezc/image/upload/v1687030696/WorldFriki/strangerthings1_jiue4t.webp" alt="Stranger Things"/></a>
     </aside>
 
     <aside className="Aside1" category="suspenso">
             <h5>The Good Doctor</h5>
-            <a href="https://www.youtube.com/watch?v=xsIFtGfAJ14" target="_blank "><img src="https://res.cloudinary.com/dhypxlezc/image/upload/v1687030696/WorldFriki/thegooddoctor_hyy6vq.jpg"/></a>
+            <a href="https://www.youtube.com/watch?v=xsIFtGfAJ14" target="_blank "><img link="https://res.cloudinary.com/dhypxlezc/image/upload/v1687030696/WorldFriki/thegooddoctor_hyy6vq.jpg"/></a>
         </aside>
 
         <aside className="Aside1" category="accion">
         <h5>Juego De Tronos</h5>
-        <a href="https://www.youtube.com/watch?v=9bwcu6rewSY" target="_blank "><img src="https://res.cloudinary.com/dhypxlezc/image/upload/v1687030688/WorldFriki/juegodetronos_hddcbs.jpg"/></a> 
+        <a href="https://www.youtube.com/watch?v=9bwcu6rewSY" target="_blank "><img link="https://res.cloudinary.com/dhypxlezc/image/upload/v1687030688/WorldFriki/juegodetronos_hddcbs.jpg"/></a> 
     </aside>
 
     <aside className="Aside1" category="suspenso">
         <h5>Pretty Little Liars</h5>
-        <a href="https://www.youtube.com/watch?v=nwHvFNQG5UE" target="_blank "><img src="https://res.cloudinary.com/dhypxlezc/image/upload/v1687030693/WorldFriki/prettylittleliars_cwx9vc.jpg"/></a> 
+        <a href="https://www.youtube.com/watch?v=nwHvFNQG5UE" target="_blank "><img link="https://res.cloudinary.com/dhypxlezc/image/upload/v1687030693/WorldFriki/prettylittleliars_cwx9vc.jpg"/></a> 
     </aside>
 
     <aside className="Aside1" category="accion">
         <h5>Peaky Blinders</h5>
-        <a href="https://www.youtube.com/watch?v=H282c9q2MUU" target="_blank "><img src="https://res.cloudinary.com/dhypxlezc/image/upload/v1687030698/WorldFriki/peakybleanders_hsppqo.webp"/></a> 
+        <a href="https://www.youtube.com/watch?v=H282c9q2MUU" target="_blank "><img link="https://res.cloudinary.com/dhypxlezc/image/upload/v1687030698/WorldFriki/peakybleanders_hsppqo.webp"/></a> 
     </aside>
 
     <aside className="Aside1" category="suspenso">
         <h5>The End Of The F***ing World</h5>
-        <a href="https://www.youtube.com/watch?v=tJqIvhprg10" target="_blank "><img src="https://res.cloudinary.com/dhypxlezc/image/upload/v1687030696/WorldFriki/fuckingworld_tfbimu.webp"/></a> 
+        <a href="https://www.youtube.com/watch?v=tJqIvhprg10" target="_blank "><img link="https://res.cloudinary.com/dhypxlezc/image/upload/v1687030696/WorldFriki/fuckingworld_tfbimu.webp"/></a> 
     </aside>
 
     <aside className="Aside1" category="suspenso">
         <h5>Riverdale</h5>
-        <a href="https://www.youtube.com/watch?v=oTfzt6C7PYw" target="_blank "><img src="https://res.cloudinary.com/dhypxlezc/image/upload/v1687030697/WorldFriki/riverdale_cbpi38.jpg"/></a> 
+        <a href="https://www.youtube.com/watch?v=oTfzt6C7PYw" target="_blank "><img link="https://res.cloudinary.com/dhypxlezc/image/upload/v1687030697/WorldFriki/riverdale_cbpi38.jpg"/></a> 
     </aside>
 
     <aside className="Aside1" category="suspenso">
         <h5>Gambito De Dama</h5>
-        <a href="https://www.youtube.com/watch?v=-BBgzgNgzeQ" target="_blank "><img src="https://res.cloudinary.com/dhypxlezc/image/upload/v1687030686/WorldFriki/gambitodedama_qmlxnt.jpg"/></a> 
+        <a href="https://www.youtube.com/watch?v=-BBgzgNgzeQ" target="_blank "><img link="https://res.cloudinary.com/dhypxlezc/image/upload/v1687030686/WorldFriki/gambitodedama_qmlxnt.jpg"/></a> 
     </aside>
     
     <aside className="Aside1" category="accion">
         <h5>The Sex Education</h5>
-        <a href="https://www.youtube.com/watch?v=1ATuTj8cBy8" target="_blank "><img src="https://res.cloudinary.com/dhypxlezc/image/upload/v1687030695/WorldFriki/sexeducation_ulsh1v.jpg"/></a> 
+        <a href="https://www.youtube.com/watch?v=1ATuTj8cBy8" target="_blank "><img link="https://res.cloudinary.com/dhypxlezc/image/upload/v1687030695/WorldFriki/sexeducation_ulsh1v.jpg"/></a> 
     </aside>
 
     <aside className="Aside1" category="suspenso">
         <h5>Euphoria</h5>
-        <a href="https://www.youtube.com/watch?v=nqSQUrlnB9s" target="_blank "><img src="https://res.cloudinary.com/dhypxlezc/image/upload/v1687030693/WorldFriki/EUPHORIA_brtm3y.jpg"/></a> 
+        <a href="https://www.youtube.com/watch?v=nqSQUrlnB9s" target="_blank "><img link="https://res.cloudinary.com/dhypxlezc/image/upload/v1687030693/WorldFriki/EUPHORIA_brtm3y.jpg"/></a> 
     </aside>
 
     <aside className="Aside1" category="accion">
         <h5>Elite</h5>
-        <a href="https://www.youtube.com/watch?v=4rTBAlB1JkQ" target="_blank "><img src="https://res.cloudinary.com/dhypxlezc/image/upload/v1687030691/WorldFriki/ELITE_nrs3di.jpg"/></a> 
+        <a href="https://www.youtube.com/watch?v=4rTBAlB1JkQ" target="_blank "><img link="https://res.cloudinary.com/dhypxlezc/image/upload/v1687030691/WorldFriki/ELITE_nrs3di.jpg"/></a> 
     </aside>
 
     <aside className="Aside1" category="suspenso">
         <h5>Mr. Robot</h5>
-        <a href="https://www.youtube.com/watch?v=NcgfoRpWLTs" target="_blank "><img src="https://res.cloudinary.com/dhypxlezc/image/upload/v1687030691/WorldFriki/mr.robot_fmmddv.jpg"/></a> 
+        <a href="https://www.youtube.com/watch?v=NcgfoRpWLTs" target="_blank "><img link="https://res.cloudinary.com/dhypxlezc/image/upload/v1687030691/WorldFriki/mr.robot_fmmddv.jpg"/></a> 
     </aside>
 
     <aside className="Aside1" category="terror">
         <h5>La Maldicion De La hillhouse</h5>
-        <a href="https://www.youtube.com/watch?v=mTvNeafShH0" target="_blank "><img src="https://res.cloudinary.com/dhypxlezc/image/upload/v1687030689/WorldFriki/maldicion_p9agyi.jpg"/></a>  
+        <a href="https://www.youtube.com/watch?v=mTvNeafShH0" target="_blank "><img link="https://res.cloudinary.com/dhypxlezc/image/upload/v1687030689/WorldFriki/maldicion_p9agyi.jpg"/></a>  
     </aside>
 
     <aside className="Aside1" category="accion">
         <h5>La chica nueva</h5>
-        <a href="https://www.youtube.com/watch?v=TKbLLGRcbcI" target="_blank "><img src="https://res.cloudinary.com/dhypxlezc/image/upload/v1687030688/WorldFriki/lachicanueva_lqbimx.jpg"/></a> 
+        <a href="https://www.youtube.com/watch?v=TKbLLGRcbcI" target="_blank "><img link="https://res.cloudinary.com/dhypxlezc/image/upload/v1687030688/WorldFriki/lachicanueva_lqbimx.jpg"/></a> 
     </aside> 
 
     <aside className="Aside1" category="accion">
         <h5>Vagabond</h5>
-        <a href="https://www.youtube.com/watch?v=tyhnEHCUtv4" target="_blank "><img src="https://res.cloudinary.com/dhypxlezc/image/upload/v1687030686/WorldFriki/VAGABOND_et2h0y.webp"/></a> 
+        <a href="https://www.youtube.com/watch?v=tyhnEHCUtv4" target="_blank "><img link="https://res.cloudinary.com/dhypxlezc/image/upload/v1687030686/WorldFriki/VAGABOND_et2h0y.webp"/></a> 
     </aside>
 
     <aside className="Aside1" category="suspenso">
             <h5>Young Royals</h5>
-            <a href="https://www.youtube.com/watch?v=xsIFtGfAJ14" target="_blank "><img src="https://res.cloudinary.com/dhypxlezc/image/upload/v1729628548/uGGAoM8ojRHOXj16n6xWiccKr34_fd18kq.jpg"/></a>
+            <a href="https://www.youtube.com/watch?v=xsIFtGfAJ14" target="_blank "><img link="https://res.cloudinary.com/dhypxlezc/image/upload/v1729628548/uGGAoM8ojRHOXj16n6xWiccKr34_fd18kq.jpg"/></a>
         </aside>
 
     <SliderSeries />
